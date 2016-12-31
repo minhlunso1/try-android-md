3e5d0dfb248ffaa49c36d09081408a5f6a315f3b
Minh <aminh1603@gmail.com>
2016-12-31 17:03:51 +0700

    format_local_date

--- app/src/main/java/minhna/materialdesignbootsrap/activity/MainActivity.java
+++ app/src/main/java/minhna/materialdesignbootsrap/activity/MainActivity.java
@@ -2,10 +2,24 @@ package minhna.materialdesignbootsrap.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.annotation.Keep;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.jakewharton.threetenabp.AndroidThreeTen;
+
+import org.threeten.bp.LocalDate;
+import org.threeten.bp.LocalDateTime;
+import org.threeten.bp.format.DateTimeFormatter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import java.util.stream.Collectors;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -24,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        AndroidThreeTen.init(this);
         switch (getIntent().getIntExtra("id", 0)) {
             case Status.NORMAL:
                 tvTitle.setText(Category.NORMAL);
@@ -31,6 +46,23 @@ public class MainActivity extends AppCompatActivity {
             case Status.UNNORMAL:
                 tvTitle.setText(Category.UNNORMAL);
                 break;
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        LocalDate now = LocalDate.now();//only date, no time part
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        String date = now.format(formatter);
+        Toast.makeText(this, "Current Date:"+ date, Toast.LENGTH_SHORT).show();
+
+        List<Integer> num = new ArrayList<>();
+        num.addAll(Arrays.asList(1,2,3,4));
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.N) {
+            List<Integer> strings = num.stream().filter(n -> n % 2 == 0).sorted().collect(Collectors.toList());
+            for (Integer myInt : strings)
+                System.out.println(myInt);
         }
     }
 
